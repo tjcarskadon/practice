c79aa1008aaca2cc88578186cc639bc5d6aa06e0
Tj Carskadon <tjcarskadon@gmail.com>
2016-03-23 08:57:45 +0800

    refactor to remove comments and change the numWords object to an array instead

--- hr42/practice/numToText.js
+++ hr42/practice/numToText.js
@@ -1,47 +1,31 @@
 function numToText(input) {
-//create some kind of object or holder for the numbers to match
-//concatenate numbers over 20 and again over 100
-//write this first with a internal helper function 
-
-//split the first letter, check it.  If it isn't a number then 
-//concate it to the result string.  If it is a number then get the word 
-//version of the number.  Then call the function again with the input.
-//base case is if the input string is empty.
-
-//use a regex to see if the value is a number.  
-
-var numWords = {
-  0: "zero",
-  1: "one",
-  2: "two",
-  3: "three",
-  4: "four",
-  5: "five",
-  6: "six",
-  7: "seven",
-  8: "eight",
-  9: "nine",
-  10: "ten"
-};
+ 
+var nums = ["zero","one","two","three","four","five","six","seven","eight","nine"];
 var result ="";
-  function parse(str) {
+var prevChar = "";
+var wasNum = false;
+ function parse(str) {
     var checkMe = str.slice(0,1);
     str = str.slice(1,str.length);
     
     if(checkMe === '') {
-      console.log(result);
       return result;
     }
 
     if(checkMe.search(/\d/) !== -1) {
-        result+= numWords[+checkMe];
+        result+= nums[+checkMe];
+
     } else {
-      result += checkMe;      
+      result += checkMe;
     }
     
+    prevChar = checkMe;
     parse(str);
 
   }
 parse(input);
+return result;
 }
-numToText("h3ll0");
+
+console.log(numToText("h3ll0"));
+console.log(numToText("1234567809"));
