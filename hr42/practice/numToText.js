0afffff37a00e711206012292ade161c89c07527
Tj Carskadon <tjcarskadon@gmail.com>
2016-03-25 17:40:29 +0800

    implement a version where result is correct but returns undefined

--- hr42/practice/numToText.js
+++ hr42/practice/numToText.js
@@ -1,28 +1,24 @@
-function numToText(input) {
+function numToText(input,result) {
  
   var nums = ["zero","one","two","three","four","five","six","seven","eight","nine"];
-  var result ="";
+  result = result || "";
 
   var checkMe = input.slice(0,1);
   input = input.slice(1,input.length);
-  
-  if(checkMe === '') {
-    return result;
-  }
 
   if(checkMe.search(/\d/) !== -1) {
       result+= nums[+checkMe];
-
   } else {
     result += checkMe;
   }
-  
-  numToText(input);
-
+    
+  if(input === '') {
+    console.log(result);
+    return result;
+  } else {
+    numToText(input, result);
   }
 
-return result;
 }
-
 console.log(numToText("h3ll0"));
-// console.log(numToText("1234567809"));
+console.log(numToText("1234567809"));
