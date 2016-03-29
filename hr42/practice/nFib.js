85d36b84cce3be12537719804e4210965b6475c9
Tj Carskadon <tjcarskadon@gmail.com>
2016-03-29 17:29:05 +0800

    implement nFib.js

---
+++ hr42/practice/nFib.js
@@ -0,0 +1,23 @@
+//a function that will return the fibonacci number at the nth location.
+
+
+function nFib(num,fib) {
+  fib = fib || [0,1];
+  var fibL = fib.length;
+
+  if(num <= 1 && fibL <=2) {
+    return fib[num];
+  }
+
+  //base case
+  if(num === 1) {
+    console.log(fib[fibL-1]);
+    return fib[fibL-1];
+  }
+
+  fib.push(fib[fibL-1] + fib[fibL-2]);
+  nFib(num-1,fib);
+}
+
+
+console.log(nFib(2)); //2
